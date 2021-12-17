4ccb2235d4c8519894d42a1c1014e7088a46c9eb
Yujing <yujing24700@gmail.com>
2021-12-17 09:44:30 +0800

    Big 0 notation descibe

---
+++ algorithms/bingo.py
@@ -0,0 +1,7 @@
+# Big 0 notation (time/space complexity)
+#how we measure and compare efficiency of algorithms
+#common Big-0 values:
+#0(1) - constant space/time
+#0(n) - linear space/time
+#0(log n) - logarithmic space/time (merge and conquer)
+#o(n^2) - quadratic space/time (nested loops)
