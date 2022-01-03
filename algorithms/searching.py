efba267551a3b84023014c192cb9f038bdb3db0b
Yujing <yujing24700@gmail.com>
2022-01-03 11:57:52 +0800

    binary search

--- algorithms/searching.py
+++ algorithms/searching.py
@@ -1,8 +1,17 @@
 sorted_list = [1, 4, 6, 9, 11, 14, 15, 16, 18]
 target = 4
 def binary_search(sorted_list, target):
+     #setup of the algorithm - getting the pointers ready
     left_idx = 0
     right_idx = len(sorted_list, -1)
+
+
+    #repeat steps - 
+    #1. calculate the middle between two points
+    #2. check if the value at the middle index equals our target
+    #3. if no and target is greater than middle value, update (move) the 
+    #4. if no and target is less than middle value, update (move) the l
+    #5. if the value doesn't exist in the list, return -1
     
     while(left_idx <= right_idx):
         middle_idx = len(sorted_list) / 2 #calculate middle indes position
@@ -14,6 +23,8 @@ def binary_search(sorted_list, target):
         else:
              left_idx = middle_idx +1
     return -1 #if the value isn't in the list, return -1
+
+
 
 
 result = binary_search(sorted_list, target)
