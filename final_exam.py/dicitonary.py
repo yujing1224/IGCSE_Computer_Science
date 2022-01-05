9ea35a05d31d24f16fd250b04a34c2b51cf9babe
Yujing <yujing24700@gmail.com>
2022-01-05 11:50:44 +0800

    dicitonaries

---
+++ final_exam.py/dicitonary.py
@@ -0,0 +1,4 @@
+student_scores = {"Yujing":[100,99,98], "Angelina": [91,90,92]}
+student_scores = student_scores.get("Yujing")
+for score in student_scores:
+    print  (score)
---
+++ final_exam.py/list.py
@@ -0,0 +1,10 @@
+#create a list of boolean values to represent
+#students who have done their homework
+has_done_homework = [True, False, True, False, True, False]
+print(has_done_homework)
+has_done_homework.sort()
+print(has_done_homework)
+
+#how many false values are in this data structure
+number_Falses = has_done_homework.count(False)
+print(number_Falses)
